d3b46e9be52f66525c1f2de10a2c745b958ae3ff
juleberry <105573843+juleberry@users.noreply.github.com>
2022-10-04 14:14:02 -0400

    onclick generates random question

--- src/App.js
+++ src/App.js
@@ -21,27 +21,42 @@ function App() {
     getQuestion()
   }, [])
 
+  // const handleClick = () => {
+  //   // const ans = document.getElementsById('answerReveal')
+  //   // ans.innerText = "Answer: {question.answer}"
+  //   console.log('clicked')
+  // }
+
     return (
       <>
     <div className="App">
       <div id="intro">
       <h1 id="header">Welcome to JEOPARDY!</h1>
       </div>
-      <h3 class="mainText">Score: </h3>
-      <h3 class="mainText">Let's Play!</h3>
-      <button>Random Trivia Question</button>
+      <h3 className="mainText">Score: </h3>
+      <h3 className="mainText">Let's Play!</h3>
+      <button onClick={() => {
+        getQuestion()
+        console.log('random was clicked')
+      }}>Random Trivia Question</button>
     <div id="mainGame" className="container">
       {question.map(question => (
         <div key={question.id}>
-          <h2>Category: {question.category.title}</h2>
-          <h3>Question: {question.question}</h3>
-          <h3>Answer: {question.answer}</h3>
+          <div id="questionArea">
+          <h4>Category: <span id="categoryTitle">{question.category.title}</span></h4>
+          <p>Question: {question.question}</p>
+          </div>
+          <div id="answerReveal" onClick={() => {
+            const answer = document.getElementById("answerReveal")
+            console.log('reveal was clicked')
+          }}><h3>Click to Reveal Answer</h3>
+          </div>
       </div>))}
     </div>
-      <div id="answerReveal">Click to Reveal Answer</div>
-      <div id="revealed"><h4>Revealed Answer</h4></div>
+    {/* toggle className between "answerReveal" and "revealed" */}
+      
     </div>
-    <div><a href="https://www.freepik.com/free-vector/led-wall-video-screen-with-glowing-dot-lights_9395692.htm#query=quiz%20show&position=2&from_view=keyword">Image by upklyak</a> on Freepik</div>
+    {/* <div><a href="https://www.freepik.com/free-vector/led-wall-video-screen-with-glowing-dot-lights_9395692.htm#query=quiz%20show&position=2&from_view=keyword">Image by upklyak</a> on Freepik</div> */}
     </>
   );
 }
