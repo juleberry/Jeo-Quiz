2189b5e413c50d109c4c4708ea54c023db5abec6
juleberry <105573843+juleberry@users.noreply.github.com>
2022-10-04 15:10:54 -0400

    add hide/reveal answer functionality

--- src/App.js
+++ src/App.js
@@ -21,22 +21,20 @@ function App() {
     getQuestion()
   }, [])
 
-  // const handleClick = () => {
-  //   // const ans = document.getElementsById('answerReveal')
-  //   // ans.innerText = "Answer: {question.answer}"
-  //   console.log('clicked')
-  // }
-
     return (
       <>
     <div className="App">
       <div id="intro">
       <h1 id="header">Welcome to JEOPARDY!</h1>
       </div>
-      <h3 className="mainText">Score: </h3>
+      {/* <h3 className="mainText">Score: </h3> */}
       <h3 className="mainText">Let's Play!</h3>
       <button onClick={() => {
         getQuestion()
+        const ansBtn = document.getElementById('answerReveal')
+        const showAns = document.getElementById("answerBox")
+        ansBtn.classList.remove('hidden')
+        showAns.classList.add('hidden')
         console.log('random was clicked')
       }}>Random Trivia Question</button>
     <div id="mainGame" className="container">
@@ -47,14 +45,20 @@ function App() {
           <p>Question: {question.question}</p>
           </div>
           <div id="answerReveal" onClick={() => {
-            const answer = document.getElementById("answerReveal")
+            const ansBtn = document.getElementById('answerReveal')
+            const showAns = document.getElementById("answerBox")
+            showAns.classList.remove('hidden')
+            ansBtn.classList.add('hidden')
             console.log('reveal was clicked')
           }}><h3>Click to Reveal Answer</h3>
           </div>
+          <div id="answerBox" className="hidden">
+            {/* some answers have additional html in them; a possible fix would be to check answer for carat tags to determine if answer can generated as is or injected as html content */}
+            <span id="answer">Answer: {question.answer}</span>
+            <p>Click "Random Trivia Question" for another question</p>
+          </div>
       </div>))}
     </div>
-    {/* toggle className between "answerReveal" and "revealed" */}
-      
     </div>
     {/* <div><a href="https://www.freepik.com/free-vector/led-wall-video-screen-with-glowing-dot-lights_9395692.htm#query=quiz%20show&position=2&from_view=keyword">Image by upklyak</a> on Freepik</div> */}
     </>
