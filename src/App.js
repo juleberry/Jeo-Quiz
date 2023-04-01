44b928c13e3da3061e4ccba783139621732b69d6
juleberry <105573843+juleberry@users.noreply.github.com>
2023-03-31 21:32:04 -0400

    fix score functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./style.css";
 import "./App.css";
 import { useState, useEffect } from "react";
-import Score from "./Score";
 
 function App() {
   const [question, setQuestion] = useState([]);
+
+  const [score, setScore] = useState(0);
+
+  const handleIncrement = () => setScore(score + 100);
+  const handleDecrement = () => setScore(score - 100);
+  // must assign for reset function
+  const reset = () => {
+    setScore(0);
+  };
 
   const getQuestion = async () => {
     const api_url = "https://jservice.io/api/random?count=1";
@@ -59,7 +67,27 @@ function App() {
                 </div>
 
                 <div id="scoreArea">
-                  <Score />
+                  <h2>Score: {score}</h2>
+                  <section id="scores">
+                    <button
+                      onClick={handleIncrement}
+                      id="correctAnsBtn"
+                      className="scoreBtns"
+                    >
+                      +
+                    </button>
+                    <button
+                      onClick={handleDecrement}
+                      id="wrongAnsBtn"
+                      className="scoreBtns"
+                    >
+                      -
+                    </button>
+                    <br />
+                    <button onClick={reset} id="resetBtn" className="scoreBtns">
+                      Reset
+                    </button>
+                  </section>
                 </div>
               </div>
 
